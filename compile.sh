698a1bc225c516fce042c6a70639be9cd8654a68
Simon Leigh <simon.leigh@couchbase.com>
2016-03-08 16:22:23 +0000

    Updated Couchbase SDK

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #
-javac -classpath  ./lib/core-io-1.1.2-dp.jar:./lib/java-client-2.1.2-dp.jar -d ./bin src/com/couchbase/support/DownNodeRecoveryTester.java
+javac -classpath  ./lib/couchbase-core-io-1.1.6.jar:./lib/couchbase-java-client-2.1.6.jar -d ./bin src/com/couchbase/support/DownNodeRecoveryTester.java
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #
-java -classpath ./lib/core-io-1.1.2-dp.jar:./lib/java-client-2.1.2-dp.jar:./lib/rxjava-1.0.4.jar:./bin com.couchbase.support.DownNodeRecoveryTester 
+java -classpath ./lib/couchbase-core-io-1.1.6.jar:./lib/couchbase-java-client-2.1.6.jar:./lib/rxjava-1.0.4.jar:./bin com.couchbase.support.DownNodeRecoveryTester
